d9cdd333edc75f12c47d460608765adae3b08b0a
Ahmat293 <goukouni500@gmail.com>
2024-08-24 21:26:55 +0200

    Ajout du fichier main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+
+int main(int argc, char* argv[]){
+    printf("\t\t\tBonjour\nAujourd'hui creation d'un interpreteur de langage de programmation basique\n");
+    return 0;
+}
